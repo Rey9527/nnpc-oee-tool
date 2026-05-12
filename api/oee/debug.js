e53fc6f16e293d8bd34a2f9c4021dec2411a0241
Rey Huang <rey.huangcr@gmail.com>
2026-05-13 01:01:05 +0700

    Add OEE API debug endpoint

---
+++ api/oee/debug.js
@@ -0,0 +1,10 @@
+const common = require('../../lib/oee-common');
+
+module.exports = function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.end(JSON.stringify({
+    ok: true,
+    loaded: Boolean(common && common.json),
+  }));
+};
