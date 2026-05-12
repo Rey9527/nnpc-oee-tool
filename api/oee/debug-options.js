06e8f318302555f0ac8796397eaf99a5cbd0c799
Rey Huang <rey.huangcr@gmail.com>
2026-05-13 01:05:08 +0700

    Remove debug API endpoints

--- api/oee/debug-options.js
+++
@@ -1,6 +0,0 @@
-const { handleOptions, json } = require('../../lib/oee-common');
-
-module.exports = function handler(req, res) {
-  if (handleOptions(req, res)) return;
-  return json(res, 200, { ok: true, method: req.method });
-};
